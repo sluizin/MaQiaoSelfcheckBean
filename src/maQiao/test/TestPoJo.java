29009c5805d16ed81cb127a11ec0d7cc7bb124da
sluizin <sluizin@sohu.com>
2017-02-11 12:56:42 +0800

    添加对pojo Bean的test

---
+++ src/maQiao/test/TestPoJo.java
@@ -0,0 +1,52 @@
+/**
+ * 
+ */
+package maQiao.test;
+
+import maQiao.maQiaoSelfcheckBean.InterfaceSC;
+import maQiao.maQiaoSelfcheckBean.selfCheck;
+
+import org.junit.Test;
+
+/**
+ * @author Sunjian
+ * @version 1.0
+ * @since jdk1.7
+ */
+public class TestPoJo {
+
+	@Test
+	public void test() {
+		PojoBean pojoBean = new PojoBean();
+		System.out.println("idSafe:" + pojoBean.idSafe());
+		pojoBean.name = "sunjian";
+		pojoBean.score = 100;
+		System.out.println("idSafe:" + pojoBean.idSafe());
+	}
+
+	public static class PojoBean {
+		int id;
+		String name;
+		int score;
+
+		public boolean idSafe() {
+			return selfCheck.isSafeBean(this);
+		}
+	}
+
+	/**
+	 * 对Bean进行自检
+	 * @author Sunjian
+	 * @version 1.0
+	 * @since jdk1.8
+	 */
+	public static class PojoBeanSC implements InterfaceSC<PojoBean> {
+		public boolean Selfcheck(PojoBean value) {
+			if (value == null) return false;
+			if (value.name == null || value.name.length() == 0) return false;
+			if (value.score <= 0) return false;
+			return true;
+		}
+
+	}
+}
